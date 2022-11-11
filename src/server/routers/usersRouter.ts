241669b27acc3d3a79ff8abe9c79ec9d8f41033b
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-12 00:37:35 +0100

    Add validate registerUserSchema to registerRoute

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { validate } from "express-validation";
+import { registerUser } from "../controllers/usersControllers";
+import registerUserSchema from "../schemas/registerUserSchema";
+import routes from "./routes";
+
+const { registerRoute } = routes;
 
 // eslint-disable-next-line new-cap
 const usersRouter = Router();
+
+usersRouter.post(
+  registerRoute,
+  validate(registerUserSchema, {}, { abortEarly: false }),
+  registerUser
+);
 
 export default usersRouter;
