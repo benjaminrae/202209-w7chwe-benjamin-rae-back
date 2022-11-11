f50c89c2ab78f0f000867c1c688b20e337943586
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-11 23:00:05 +0100

    Create connectDb function

---
+++ src/database/connectDb.ts
@@ -0,0 +1,22 @@
+import environment from "../loadEnvironment";
+import mongoose from "mongoose";
+
+const { mongoDebug } = environment;
+
+const connectDb = async (mongoDbUri: string) => {
+  await mongoose.connect(mongoDbUri);
+
+  mongoose.set("debug", mongoDebug);
+
+  mongoose.set("toJSON", {
+    virtuals: true,
+    transform(doc, ret) {
+      delete ret._id;
+      delete ret.__v;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return ret;
+    },
+  });
+};
+
+export default connectDb;
--- src/loadEnvironment.ts
+++ src/loadEnvironment.ts
@@ -1,17 +1,27 @@
 import dotenv from "dotenv";
+import { mongo } from "mongoose";
 
 dotenv.config();
 
 interface Environment {
   port: number;
   debug: string;
+  mongoDebug: boolean;
+  mongoDbUri: string;
 }
 
-const { PORT: port, DEBUG: debug } = process.env;
+const {
+  PORT: port,
+  DEBUG: debug,
+  MONGO_DEBUG: mongoDebug,
+  MONGO_DB_URI: mongoDbUri,
+} = process.env;
 
 const environment: Environment = {
   port: +port,
   debug,
+  mongoDebug: mongoDebug === "true",
+  mongoDbUri,
 };
 
 export default environment;
