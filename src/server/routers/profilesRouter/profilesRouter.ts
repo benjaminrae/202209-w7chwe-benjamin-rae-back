0ded0d68aa15a283441aad2d304c0a5c8da68e6f
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-14 19:28:36 +0100

    Add upload middleware /profiles/edit route

--- src/server/routers/profilesRouter/profilesRouter.ts
+++ src/server/routers/profilesRouter/profilesRouter.ts
@@ -1,14 +1,18 @@
+import multer from "multer";
 import { Router } from "express";
 import {
   editProfile,
   getProfiles,
 } from "../../controllers/profilesControllers/profilesControllers.js";
+import path from "path";
+
+const upload = multer({ dest: path.join("assets", "images") });
 
 // eslint-disable-next-line new-cap
 const profilesRouter = Router();
 
 profilesRouter.get("", getProfiles);
 
-profilesRouter.put("/edit", editProfile);
+profilesRouter.put("/edit", upload.single("image"), editProfile);
 
 export default profilesRouter;
